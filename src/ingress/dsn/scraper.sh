f254c88ea91d1bdfe0465c5ad480913d43e4f062
Ciluvien <git@ciluvien.de>
2025-12-17 15:46:00 +0100

    <Scraper> improve archive and file names
    Files now named according to ISO 8601
    Scraper now relies on external restart on error

--- src/ingress/dsn/scraper.sh
+++ src/ingress/dsn/scraper.sh
@@ -1,43 +1,42 @@
 #!/bin/bash
 # Downloads current DSN Now XML file and archives it
 
-OUT_DIR="../../../data/raw/"
-ERROR_DIR="../../../data/errors/"
+OUT_DIR="../../../data/raw"
+ERROR_DIR="../../../data/errors"
 URL="https://eyes.nasa.gov/dsn/data/dsn.xml"
 SLEEP_DURATION=4
 
-scrape () {
-    while true; do
-        today_archive="$(date -Idate).zip"
-        path_archive="$OUT_DIR$today_archive"
-        xml_file="$(date +%s).xml"
+single_scrape () {
+    today_archive="$(date --utc -Idate).zip"
+    path_archive="$OUT_DIR/$today_archive"
+    xml_file=$(echo "$(date --utc --iso-8601=seconds).xml" | tr : _)
 
-        if curl -s -o "$xml_file" "$URL"; then
-            # Attempt to add the XML file to the zip archive
-            if zip -g "$path_archive" "$xml_file"; then
-                rm "$xml_file"
-            else
-                echo "Failed to add $xml_file to $path_archive"
-                mkdir -p "$ERROR_DIR"
-                # Move the XML file to the error directory
-                mv "$xml_file" "$ERROR_DIR"
-            fi
+    if curl -s -o "$xml_file" "$URL"; then
+        # Attempt to add the XML file to the zip archive
+        if zip -q -g "$path_archive" "$xml_file"; then
+            rm "$xml_file"
         else
-            echo "Failed to download XML file: $xml_file"
+            echo "Failed to add $xml_file to $path_archive" >&2
+            mkdir -p "$ERROR_DIR"
+            # Move the XML file to the error directory
+            mv "$xml_file" "$ERROR_DIR/"
         fi
-
-        sleep $SLEEP_DURATION
-    done
+    else
+        echo "Failed to download XML file: $xml_file" >&2
+        rm "$xml_file"
+    fi
 }
-
 
 if [ ! -d "$OUT_DIR" ]; then
     echo "Creating output directory $OUT_DIR"
     mkdir -p "$OUT_DIR"
 fi
 
-while true; do
-    scrape
-    echo "Scraper encountered an error. Restarting."
-    sleep $SLEEP_DURATION
-done
+main_loop () {
+    while true; do
+        single_scrape
+        sleep "$SLEEP_DURATION"
+    done
+}
+
+main_loop
